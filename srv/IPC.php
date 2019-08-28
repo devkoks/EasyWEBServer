aad17630cb469fa1fe44ed261ac428ae79e761fe
Alexey Kos <akos@kcs.kz>
2019-08-28 13:16:43 +0600

    IPC string keys

--- srv/IPC.php
+++ srv/IPC.php
@@ -12,28 +12,40 @@ class IPC
         if($ftok == null)
             $ftok = ftok(__FILE__,"t");
         $this->shm = shm_attach($ftok,$this->lim);
+        shm_put_var($this->shm,0,[['def']]);
     }
     public function __destruct()
     {
         $this->close();
     }
-    public function get(int $id)
+    public function get($name)
     {
-        if(!$this->isset($id)) return null;
-        return shm_get_var($this->shm,$id);
+        if(!$this->isset($this->key($name))) return null;
+        return shm_get_var($this->shm,$this->key($name));
     }
-    public function set(int $id, $var)
+    public function set($name, $var)
     {
-        shm_put_var($this->shm,$id,$var);
+        shm_put_var($this->shm,$this->key($name),$var);
     }
-    public function isset(int $id)
+    public function isset($name)
     {
-        return shm_has_var($this->shm,$id);
+        return shm_has_var($this->shm,$this->key($name));
     }
     public function close()
     {
         if(get_resource_type($this->shm)!="sysvshm") return;
         shm_remove($this->shm);
         shm_detach($this->shm);
+    }
+    private function key($name)
+    {
+        $names = shm_get_var($this->shm,0);
+        $key = array_search($name,$names);
+        if($key === false){
+            $names[] = $name;
+            shm_put_var($this->shm,0,$names);
+        }
+
+        return array_search($name,$names);
     }
 }
