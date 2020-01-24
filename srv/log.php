2a38497787d9e6701c293e613829c50e228e6f91
Кос Алексей <akos@kcs.kz>
2020-01-24 13:32:24 +0000

    Update 'srv/log.php'

--- srv/log.php
+++ srv/log.php
@@ -9,7 +9,7 @@
 function slog(string $level, string $text): void
 {
     if($_SERVER["LOGS_ENABLE"]==false) return;
-    if(!file_exists($_SERVER["CONF"]['logs'])) mkdir($_SERVER["CONF"]['logs']);
+    if(!file_exists($_SERVER["CONF"]['logs'])) mkdir($_SERVER["CONF"]['logs'],0644,true);
     $time = time();
 	$date = getdate($time);
 	$string = date("[d/m/Y H:i:s]",$time)."[".$level."]".$text.PHP_EOL;
